9d8157faf7c8c1c06b1bc66762846c6de8ac56c9
bbkiwi <staf186@ext.canterbury.ac.nz>
2017-08-11 22:35:50 +1200

    Fixed embeddedlinux to use embeddecommon code Improvements: RING option for dummy_leds More options in ccconfig.h to test extras in embeddedcommon/embeddedout.c TODO implement COLOR_CHORD_OUTPUT_DRIVER as option

--- embeddedlinux/ccconfig.h
+++ embeddedlinux/ccconfig.h
@@ -1,10 +1,21 @@
 #ifndef _CCCONFIG_H
 #define _CCCONFIG_H
 
+
 #define CCEMBEDDED
-#define NUM_LIN_LEDS 300
-#define USE_NUM_LIN_LEDS 300
+#define DEBUGPRINT 0
+#define RING
+#define LEDS_PER_ROW 16
+#define NUM_LIN_LEDS 16
+#define USE_NUM_LIN_LEDS 8
 #define DFREQ 16000
+#define NOTE_FINAL_AMP  15 
+
+#define COLORCHORD_SHIFT_INTERVAL 5    // shift after this many frames, 0 no shifts
+#define COLORCHORD_FLIP_ON_PEAK 1      // non-zero will flip on peak total amp2
+#define COLORCHORD_SHIFT_DISTANCE 1    // distance of shift + anticlockwise, - clockwise, 0 no shift
+#define COLORCHORD_SORT_NOTES 2        // 0 no sort, 1 inc freq, 2 dec amps, 3 dec amps2
+#define COLORCHORD_LIN_WRAPAROUND 0    // 0 no adjusting, else current led display has minimum deviation to prev
 
 
 #endif
--- embeddedlinux/embeddedcc.c
+++ embeddedlinux/embeddedcc.c
@@ -17,16 +17,24 @@ struct sockaddr_in servaddr;
 int sock;
 
 #define expected_lights NUM_LIN_LEDS
-
 int toskip = 1;
+int gFRAMECOUNT_MOD_SHIFT_INTERVAL = 0;
+int gROTATIONSHIFT = 0; //Amount of spinning of pattern around a LED ring
+
+//TODO explore relation of NUM_LIN_LEDS and following two params
+// ratio of COLORCHORD_SHIFT_INTERVAL / COLORCHORD_SHIFT_DISTANCE when less than 1 has interesting effect
 
 void NewFrame()
 {
 	int i;
 	char buffer[3000];
-
+        gFRAMECOUNT_MOD_SHIFT_INTERVAL++;
+	if ( gFRAMECOUNT_MOD_SHIFT_INTERVAL >= COLORCHORD_SHIFT_INTERVAL ) gFRAMECOUNT_MOD_SHIFT_INTERVAL = 0;
+	//printf("MOD FRAME %d ******\n", gFRAMECOUNT_MOD_SHIFT_INTERVAL);
 	HandleFrameInfo();
 	UpdateLinearLEDs();
+	//UpdateAllSameLEDs();
+	//UpdateRotatingLEDs();
 
 	buffer[0] = 0;
 	buffer[1] = 0;
@@ -35,6 +43,8 @@ void NewFrame()
 	for( i = 0; i < expected_lights * 3; i++ )
 	{
 		buffer[i+toskip*3] = ledOut[i];
+// printf("In NF: %d\n", ledOut[i]);
+
 	}
 
 	int r = send(sock,buffer,expected_lights*3+3,0);
@@ -45,6 +55,7 @@ int main( int argc, char ** argv )
 {
 	int wf = 0;
 	int ci;
+	// bb no [tool] argument expected or used?
 
 	if( argc < 2 )
 	{
@@ -53,7 +64,11 @@ int main( int argc, char ** argv )
 	}
 
 	printf( "%d\n", argc );
+        for (int i = 0; i < argc; i++)
+        {     printf( "%s\n", argv[i] );
+        }
 	toskip = (argc > 2)?atoi(argv[2]):0;
+        fprintf( stderr, "   toskip = %d \n", toskip );
 
 	sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
 
@@ -61,14 +76,16 @@ int main( int argc, char ** argv )
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr(argv[1]);
 	servaddr.sin_port=htons(7777);
-
+	fprintf(stderr, "The IP address is %s\n", inet_ntoa(servaddr.sin_addr.s_addr));
 	connect( sock, (struct sockaddr *)&servaddr, sizeof(servaddr) );
 
-	Init();
+	InitColorChord(); // bb changed from Init() which does not seem to exist;
 
 	while( ( ci = getchar() ) != EOF )
 	{
 		int cs = ci - 0x80;
+//printf("byte: %d\n", cs);
+// putchar(ci);
 #ifdef USE_32DFT
 		PushSample32( ((int8_t)cs)*32 );
 #else
